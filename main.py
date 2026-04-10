7fc435cec60449e295b92cd37e22ab89d63d5d31
ConsciousCode <conscious.code@gmail.com>
2026-04-09 20:35:12 -0600

    Refactored main.py to allow specifying combinations of tasks

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
-def main(which: str = 'all', *rest):
+#!/usr/bin/env python3
+
+'''
+Intro to Machine Learning Assignment 3
+Students: Jackie Javier, Pranitha Achanta, Robert McDaniels
+
+Utility script for quickly calling different tasks.
+'''
+
+def invoke(which: int, *rest):
+  print(f"### Task {which} ###")
   match which:
-    case '1':
+    case 1:
       from task1 import main
       main(*rest)
     
-    case '2':
+    case 2:
       print(f"Error: task {which} is not executable")
     
-    case '3':
+    case 3:
       from task3 import main
       main(*rest)
     
-    case '4':
+    case 4:
       from task4 import main
       main(*rest)
     
-    case '5':
+    case 5:
       from task5 import main
       main(*rest)
-    
-    case 'all':
-      if rest:
-        print("Warning: Cannot pass extra arguments to all tasks")
-      
-      import task1
-      import task3
-      import task4
-      import task5
 
-      print("### Task 1 ###")
-      task1.main()
-      print("### Task 3 ###")
-      task3.main()
-      print("### Task 4 ###")
-      task4.main()
-      print("### Task 5 ###")
-      task5.main()
+def main(which: str = 'all', *rest):
+  if which == 'all':
+    which = '1,3,4,5'
+  seen = set()
+
+  for wh in which.split(','):
+    if wh in seen:
+      print(f"Ignoring {wh}, already seen")
+    invoke(int(wh), *rest)
 
 if __name__ == "__main__":
   import sys
